fc19cf629df6cd63c7935658a9c84d74b3523952
はせ <->
2018-04-02 11:27:01 +0900

    <EController> 操作方法を判断するenum <Player>プレイヤー

---
+++ neec-kamata-team10/Assets/Script/Hasegawa/Character/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private EController eController;
+    private ICharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //controller.HorizontalMove();
+        //controller.Jump();
+	}
+}
