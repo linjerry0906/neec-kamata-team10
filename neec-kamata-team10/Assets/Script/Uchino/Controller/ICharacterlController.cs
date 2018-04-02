b1625d906a0ceee4b37a6bbfc3fd55200cd24cb3
うちの <ouyong16@gmail.com>
2018-04-02 13:30:11 +0900

    PadControllerの作成

--- neec-kamata-team10/Assets/Script/Uchino/Controller/ICharacterlController.cs
+++ neec-kamata-team10/Assets/Script/Uchino/Controller/ICharacterlController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public interface ICharacterController 
 {
-    Vector3 HorizontalMove();
-    bool    Jump();
+    Vector3 HorizontalMove(); //水平移動
+    bool Jump();              //ジャンプ
+    bool SwitchToTheRight();  //鏡を右に切り替える
+    bool SwitchToTheLeft();   //鏡を左に切り替える
+    bool OperateTheMirror();　//鏡の操作
+
 }
--- neec-kamata-team10/Assets/Script/Uchino/Controller/PadController.cs
+++ neec-kamata-team10/Assets/Script/Uchino/Controller/PadController.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeboardController : MonoBehaviour,ICharacterController
+public class PadController : MonoBehaviour,ICharacterController
 {
-    /// <summary>
-    /// 水平移動
-    /// </summary>
-    /// <returns></returns>
     public Vector3 HorizontalMove()
     {
         Vector3 velocity = Vector3.zero;
 
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetAxisRaw("Horizontal") == -1)
         {
             velocity = new Vector3(-1, 0, 0);
         }
-        if(Input.GetKey(KeyCode.D))
+        if (Input.GetAxisRaw("Horizontal") == 1)
         {
             velocity = new Vector3(1, 0, 0);
         }
@@ -30,7 +26,7 @@ public class KeboardController : MonoBehaviour,ICharacterController
     /// <returns></returns>
     public bool SwitchToTheRight()
     {
-        if(Input.GetKeyDown((KeyCode.RightArrow)))
+        if (Input.GetKeyDown(KeyCode.JoystickButton5))
         {
             return true;
         }
@@ -44,21 +40,7 @@ public class KeboardController : MonoBehaviour,ICharacterController
     /// <returns></returns>
     public bool SwitchToTheLeft()
     {
-        if (Input.GetKeyDown((KeyCode.LeftArrow)))
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    /// <summary>
-    /// 鏡の操作　(新規設置:鏡を持つ:鏡を置く)
-    /// </summary>
-    /// <returns></returns>
-    public bool OperateTheMirror()
-    {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.JoystickButton4))
         {
             return true;
         }
@@ -72,13 +54,28 @@ public class KeboardController : MonoBehaviour,ICharacterController
     /// <returns></returns>
     public bool Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.JoystickButton0))
         {
             return true;
         }
 
         return false;
     }
+
+    /// <summary>
+    /// 鏡の操作　(新規設置:鏡を持つ:鏡を置く)
+    /// </summary>
+    /// <returns></returns>
+    public bool OperateTheMirror()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton2))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
 
 
 }
