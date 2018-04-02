2aa9682c14b7acf052d6243d180e6804257a10b4
リン <linjerry0906@gmail.com>
2018-04-02 11:23:03 +0900

    GameManager追加

---
+++ neec-kamata-team10/Assets/Script/リン/GameObject/GameManager/ControllerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager
+{
+    private KeboardController keyboard_controller;
+
+    public ControllerManager()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        keyboard_controller = new KeboardController();
+    }
+
+    public ICharacterController KeyboardController()
+    {
+        return keyboard_controller;
+    }
+}
