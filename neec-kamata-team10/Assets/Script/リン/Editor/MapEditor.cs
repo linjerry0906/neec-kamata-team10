71455093e3c16ce45a16bbd7c9e63e8a735d84ce
リン <linjerry0906@gmail.com>
2018-04-16 16:14:41 +0900

    MapEditor作業保存

--- neec-kamata-team10/Assets/Script/リン/Editor/MapEditor.cs
+++ neec-kamata-team10/Assets/Script/リン/Editor/MapEditor.cs
@@ -4,10 +4,9 @@
 // 内容：マップ編集用拡張機能
 //------------------------------------------------------
 using System.Collections;
+using System.IO;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEditor;
-using UnityEditor.SceneManagement;
 
 public class MapEditor : EditorWindow
 {
@@ -22,8 +21,8 @@ public class MapEditor : EditorWindow
     private enum Mode
     {
         EditMode = 0,       //マップ編集
-        SaveLoad,           //データ保存など
         Chips,              //マップチップ関連
+        SaveLoad,           //データ保存など
     }
 
     private int currentMode = (int)Mode.EditMode;       //編集モード
@@ -34,6 +33,13 @@ public class MapEditor : EditorWindow
     private string saveFileName;                        //保存名前
     private string savePath;                            //保存場所
 
+    private float tileSize = 20;                        //タイルのサイズ
+    private int xTile = 0;                              //マップの横サイズ
+    private int yTile = 0;                              //マップの縦サイズ
+
+    private string currentTilePath = "";                //現在選択のチップ
+    private Vector2 scrollPos = Vector2.zero;           //Scroll Position
+
     /// <summary>
     /// GUI配置
     /// </summary>
@@ -43,18 +49,18 @@ public class MapEditor : EditorWindow
         EditorGUILayout.LabelField("マップ エディター");
 
         currentMode = GUILayout.Toolbar(currentMode,
-            new string[] { "マップ編集", "書出し / 読み込み", "マップチップ" });
+            new string[] { "マップ編集", "マップチップ", "書出し / 読み込み" });
 
         switch ((Mode)currentMode)                      //モードにより機能が変わる
         {
             case Mode.EditMode:                         //マップ編集モード
                 EditMap();
                 break;
-            case Mode.SaveLoad:                         //マップ保存や読み込み
-                SaveLoad();
-                break;
             case Mode.Chips:                            //マップチップの読み込み
                 MapChips();
+                break;
+            case Mode.SaveLoad:                         //マップ保存や読み込み
+                SaveLoad();
                 break;
         }
         EditorGUILayout.EndVertical();
@@ -71,24 +77,87 @@ public class MapEditor : EditorWindow
 
     private void EditMap()
     {
-        OpenEditWindow();
+        TileInfo();                                             //Mapの大きさを設定
+
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        OpenEditWindow();                                       //Viewerを開く
+        ResizetMap();                                           //マップサイズ調整
+        NewMap();                                               //新しいマップ作成
+        EditorGUILayout.EndVertical();
     }
 
+    /// <summary>
+    /// EditWindowを開く
+    /// </summary>
     private void OpenEditWindow()
     {
-        EditorGUILayout.BeginVertical(GUI.skin.box);
-        if (GUILayout.Button("Map Viewerを開く"))
+        if (GUILayout.Button("Map Viewerを開く"))               //開くボタン
         {
-            if (mapViewer == null)
+            if (mapViewer == null)                              //開いてない場合
             {
-                mapViewer = MapViewer.CreateMapViewer(this);
+                mapViewer = MapViewer.CreateMapViewer(this);    //開く
+                mapViewer.Initialize(tileSize, xTile, yTile);   //初期化
             }
             else
             {
-                mapViewer.Focus();
+                mapViewer.Focus();                              //Windowを最上階にする
             }
         }
+    }
+
+    /// <summary>
+    /// マップの大きさを設定
+    /// </summary>
+    private void TileInfo()
+    {
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+
+        EditorGUILayout.BeginHorizontal();                                           //横並び
+        EditorGUILayout.LabelField("グリッドサイズ：", GUILayout.Width(infoLength)); //説明欄
+        tileSize = EditorGUILayout.FloatField(tileSize);                             //入力欄
+        EditorGUILayout.EndHorizontal();                                             //横並び
+
+        EditorGUILayout.BeginHorizontal();                                           //横並び
+        EditorGUILayout.LabelField("横ブロック数：", GUILayout.Width(infoLength));   //説明欄
+        xTile = EditorGUILayout.IntField(xTile);                                     //入力欄
+        EditorGUILayout.EndHorizontal();                                             //横並び
+
+        EditorGUILayout.BeginHorizontal();                                           //横並び
+        EditorGUILayout.LabelField("縦ブロック数：", GUILayout.Width(infoLength));   //説明欄
+        yTile = EditorGUILayout.IntField(yTile);                                     //入力欄
+        EditorGUILayout.EndHorizontal();                                             //横並び
+
         EditorGUILayout.EndVertical();
+    }
+
+    /// <summary>
+    /// Mapの大きさを適用
+    /// </summary>
+    private void ResizetMap()
+    {
+        if (!GUILayout.Button("マップサイズを適用"))                                 //リサイズボタン
+            return;
+
+        if (mapViewer == null)                                                       //Viewer開いてない場合
+            mapViewer = MapViewer.CreateMapViewer(this);                             //開く
+
+        mapViewer.Initialize(tileSize, xTile, yTile);                                //初期化
+        mapViewer.Focus();
+    }
+
+    /// <summary>
+    /// 新しいマップ作成
+    /// </summary>
+    private void NewMap()
+    {
+        if (!GUILayout.Button("新しいマップ作成"))                                   //マップチップ作成ボタン
+            return;
+
+        if (mapViewer == null)                                                       //Viewer開いてない場合
+            mapViewer = MapViewer.CreateMapViewer(this);                             //開く
+
+        mapViewer.NewMap(tileSize, xTile, yTile);                                    //新しく生成
+        mapViewer.Focus();
     }
 
     /// <summary>
@@ -96,8 +165,8 @@ public class MapEditor : EditorWindow
     /// </summary>
     private void SaveLoad()
     {
-        Save();                                         //セーブUI
-        Load();                                         //ロードUI
+        Save();                                                                      //セーブUI
+        Load();                                                                      //ロードUI
     }
 
     private void Load()
@@ -180,5 +249,71 @@ public class MapEditor : EditorWindow
 
     private void MapChips()
     {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUI.skin.box);
+        DrawMapChip();                                                              //MapChip一覧を描画
+        EditorGUILayout.EndScrollView();
+    }
+
+    /// <summary>
+    /// マップチップ一覧
+    /// </summary>
+    private void DrawMapChip()
+    {
+        string path = "Assets/MapChip";                                             //アセットパス
+        string[] textureNames = Directory.GetFiles(path, "*.png");                  //Pngファイルを探す
+
+        float xPos = 0.0f;                                      //セルの左上X座標
+        float yPos = 0.0f;                                      //セルの左上Y座標
+        float cellWidth = 100.0f;                               //セルの横サイズ
+        float cellHeight = 100.0f;                              //セルの縦サイズ
+        int maxCell = 5;                                        //セルの縦サイズ
+
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        #region Null Chip
+        EditorGUILayout.BeginHorizontal();                      //横並び始め
+        if (GUILayout.Button("Null", GUILayout.MaxWidth(cellWidth), GUILayout.MaxHeight(cellHeight),
+                GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false)))                           //ボタン設置（テクスチャ表示）
+            currentTilePath = "Null";                           //クリックされたらテクスチャ指定
+        xPos += cellWidth;                                      //X座標を右にずらす
+        #endregion
+
+        foreach (string textureDir in textureNames)             //アセットを一周する
+        {
+            if (xPos > (maxCell - 1) * cellWidth)               //横の数を超えた場合（0から始まるので-1）
+            {
+                xPos = 0.0f;                                    //X座標をリセット
+                yPos += cellHeight;                             //Y座標を一段下にずらす
+                EditorGUILayout.EndHorizontal();                //横並び終了
+            }
+
+            if (xPos == 0.0f)                                   //最初のセルの場合
+                EditorGUILayout.BeginHorizontal();              //横並び始め
+
+            GUILayout.FlexibleSpace();
+            Texture2D tex = (Texture2D)AssetDatabase.LoadAssetAtPath(textureDir, typeof(Texture2D));    //テクスチャを読み込む
+
+            if (GUILayout.Button(tex, GUILayout.MaxWidth(cellWidth), GUILayout.MaxHeight(cellHeight),
+                GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false)))                           //ボタン設置（テクスチャ表示）
+                currentTilePath = textureDir;                   //クリックされたらテクスチャ指定
+            GUILayout.FlexibleSpace();
+            xPos += cellWidth;                                  //X座標を右にずらす
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.LabelField("Current Tile：" + currentTilePath);                                //現在選択しているチップ
+        if (currentTilePath != "Null" || currentTilePath != "")                                        //選択している場合
+        {
+            Texture2D currentTile = (Texture2D)AssetDatabase.LoadAssetAtPath(currentTilePath, typeof(Texture2D));
+            GUILayout.Box(currentTile);
+        }
+    }
+
+    /// <summary>
+    /// 現在選択のチップ
+    /// </summary>
+    /// <returns></returns>
+    public string CurrentChip()
+    {
+        return currentTilePath;
     }
 }
--- neec-kamata-team10/Assets/Script/リン/Editor/MapViewer.cs
+++ neec-kamata-team10/Assets/Script/リン/Editor/MapViewer.cs
@@ -8,10 +8,12 @@ public class MapViewer : EditorWindow
     private static int WINDOW_WIDTH = 300;
     private static int WINDOW_HEIGHT = 200;
     private MapEditor parent;
+    private Vector2 scrollPos;
 
     private float gridSize;                     //グリッドサイズ
 
-    private Rect[,] mapGrid;                    //マップグリッド
+    private List<List<Rect>> mapGrid;           //マップグリッド
+    private string[,] mapChip;                  //マップチップ
 
     public static MapViewer CreateMapViewer(MapEditor parent)
     {
@@ -41,6 +43,27 @@ public class MapViewer : EditorWindow
     {
         this.gridSize = gridSize;               //グリッドサイズを記録
         mapGrid = CreateGrid(width, height);    //マップグリッドを作成
+        InitMapChip(width, height);             //マップチップ作成
+    }
+
+    /// <summary>
+    /// 新しいマップ作成
+    /// </summary>
+    /// <param name="gridSize">グリッドサイズ</param>
+    /// <param name="width">横タイル数</param>
+    /// <param name="height">縦タイル数</param>
+    public void NewMap(float gridSize, int width, int height)
+    {
+        this.gridSize = gridSize;               //グリッドサイズを記録
+        mapGrid = CreateGrid(width, height);    //マップグリッドを作成
+        mapChip = new string[height, width];    //実体生成
+    }
+
+    private void OnGUI()
+    {
+        DrawGrid();                             //グリッド描画
+        CheckMouseClick();                      //クリックされたかをチェック
+        DrawMap();                              //マップチップ描画
     }
 
     /// <summary>
@@ -49,25 +72,148 @@ public class MapViewer : EditorWindow
     /// <param name="width">横タイル数</param>
     /// <param name="height">縦タイル数</param>
     /// <returns></returns>
-    private Rect[,] CreateGrid(int width, int height)
+    private List<List<Rect>> CreateGrid(int width, int height)
     {
         float xPos = 0.0f;                      //タイルの左上X座標
         float yPos = 0.0f;                      //タイルの左上Y座標
 
-        Rect[,] destRects = new Rect[height, width];    //指定のチップ数の配列を確保
+        List<List<Rect>> destRects = new List<List<Rect>>();            //List作成
 
         for (int y = 0; y < height; ++y)        //各グリッドを作成していく
         {
+            destRects.Add(new List<Rect>());    //一列追加
             xPos = 0.0f;                        //Xを先端にする
             for (int x = 0; x < width; ++x)     //X軸を回していく
             {
                 Rect grid = new Rect(xPos, yPos, gridSize, gridSize);  //グリッドを一個作成
-                destRects[y, x] = grid;                                //配列に入れる
+                destRects[y].Add(grid);                                //配列に入れる
                 xPos += gridSize;               //X座標をグリッドサイズにずらす
             }
             yPos += gridSize;                   //Y座標をグリッドサイズにずらす
         }
 
         return destRects;                       //作成したグリッドを返す
+    }
+
+    /// <summary>
+    /// マップチップ作成
+    /// </summary>
+    /// <param name="width">横サイズ</param>
+    /// <param name="height">縦サイズ</param>
+    private void InitMapChip(int width, int height)
+    {
+        if (mapChip == null)                                //初期化されてない状態
+        {
+            mapChip = new string[height, width];            //実体生成
+            return;
+        }
+
+        string[,] newChip = new string[height, width];      //新しいマップチップ生成
+        for (int y = 0; y < mapChip.GetLength(0); ++y)      //現在あったチップを一周
+        {
+            if (y >= height)                                //新しいチップの縦サイズを超えたら脱出
+                break;
+            for (int x = 0; x < mapChip.GetLength(1); ++x)
+            {
+                if (x >= width)                             //新しいチップの横サイズを超えたら脱出
+                    break;
+                newChip[y, x] = mapChip[y, x];              //チップを保存
+            }
+        }
+        mapChip = newChip;                                  //旧チップを新チップに指定
+    }
+
+    /// <summary>
+    /// マウスクリックをチェック
+    /// </summary>
+    private void CheckMouseClick()
+    {
+        Event e = Event.current;                            //現在のイベント取得
+        if (e.type == EventType.MouseDown)                  //マウスがクリックされたら
+        {
+            Vector2 mousePos = Event.current.mousePosition; //マウスの位置を取得
+            int xPos = 0;
+            for (xPos = 0; xPos < mapGrid[0].Count; ++xPos) //グリッドのX軸をチェック
+            {
+                Rect grid = mapGrid[0][xPos];
+                if (grid.x <= mousePos.x && mousePos.x <= grid.x + grid.width)  //範囲内なら現在のX座標を保つので、脱出
+                    break;
+            }
+
+            if (xPos >= mapGrid[0].Count)                       //Indexを超えた処理
+                xPos = mapGrid[0].Count - 1;
+
+            for (int yPos = 0; yPos < mapGrid.Count; ++yPos)    //Y軸をチェック
+            {
+                if (mapGrid[yPos][xPos].Contains(mousePos))     //指定のグリッドぼ場合
+                {
+                    mapChip[yPos, xPos] = parent.CurrentChip();
+                    Repaint();                                  //描画更新
+                    break;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// グリッド描画
+    /// </summary>
+    private void DrawGrid()
+    {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        for (int y = 0; y < mapGrid.Count; ++y)                 //Y軸
+        {
+            for (int x = 0; x < mapGrid[y].Count; ++x)          //X軸
+            {
+                DrawGridLine(mapGrid[y][x]);                    //描画
+            }
+        }
+        EditorGUILayout.EndScrollView();
+    }
+
+    /// <summary>
+    /// グリッドラインを描画
+    /// </summary>
+    /// <param name="rect">グリッド</param>
+    private void DrawGridLine(Rect rect)
+    {
+        Handles.color = new Color(1f, 1f, 1f, 0.5f);            //線の色
+
+        Handles.DrawLine(                                       //上
+            new Vector2(rect.position.x, rect.position.y),
+            new Vector2(rect.position.x + rect.size.x, rect.position.y));
+
+        Handles.DrawLine(                                       //下
+            new Vector2(rect.position.x, rect.position.y + rect.size.y),
+            new Vector2(rect.position.x + rect.size.x, rect.position.y + rect.size.y));
+
+        Handles.DrawLine(                                       //左
+            new Vector2(rect.position.x, rect.position.y),
+            new Vector2(rect.position.x, rect.position.y + rect.size.y));
+
+        Handles.DrawLine(                                       //右
+            new Vector2(rect.position.x + rect.size.x, rect.position.y),
+            new Vector2(rect.position.x + rect.size.x, rect.position.y + rect.size.y));
+    }
+
+    /// <summary>
+    /// マップチップ描画
+    /// </summary>
+    private void DrawMap()
+    {
+        for (int y = 0; y < mapChip.GetLength(0); ++y)
+        {
+            for (int x = 0; x < mapChip.GetLength(1); ++x)
+            {
+                if (mapChip[y, x] == null || mapChip[y, x] == "Null" || mapChip[y, x] == "")
+                    continue;
+
+                if (mapChip[y, x].Length > 0)
+                {
+                    Texture2D tex = (Texture2D)AssetDatabase.LoadAssetAtPath(mapChip[y, x], typeof(Texture2D));
+                    GUI.DrawTexture(mapGrid[y][x], tex);
+                }
+            }
+        }
     }
 }
