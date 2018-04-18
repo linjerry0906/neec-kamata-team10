9055eac9d64c5b7fd5e4c4f7c3c5885d08e2910e
リン <linjerry0906@gmail.com>
2018-04-18 12:13:04 +0900

    マップエディター修正

--- neec-kamata-team10/Assets/Script/リン/Editor/MapEditor.cs
+++ neec-kamata-team10/Assets/Script/リン/Editor/MapEditor.cs
@@ -6,7 +6,10 @@
 using System.Collections;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 public class MapEditor : EditorWindow
 {
@@ -14,31 +17,26 @@ public class MapEditor : EditorWindow
     [MenuItem("Window/マップ編集")]
     static void Open()
     {
-        //Window表示（すでにある場合は既存のWindowを最上階）
-        GetWindow<MapEditor>("マップ編集");
+        GetWindow<MapEditor>("マップ編集");             //Window表示（すでにある場合は既存のWindowを最上階）
     }
 
     private enum Mode
     {
         EditMode = 0,       //マップ編集
-        Chips,              //マップチップ関連
         SaveLoad,           //データ保存など
     }
 
     private int currentMode = (int)Mode.EditMode;       //編集モード
-    private MapViewer mapViewer;
 
     private int infoLength = 100;                       //説明欄の長さ
 
     private string saveFileName;                        //保存名前
-    private string savePath;                            //保存場所
+    private string savePath = "Assets/";                //保存場所
 
-    private float tileSize = 20;                        //タイルのサイズ
-    private int xTile = 0;                              //マップの横サイズ
-    private int yTile = 0;                              //マップの縦サイズ
+    private GameObject tileMapPrefab;                   //TileMapのPrefab
+    private GameObject gameManagerPrefab;               //GameManagerのPrefab
 
-    private string currentTilePath = "";                //現在選択のチップ
-    private Vector2 scrollPos = Vector2.zero;           //Scroll Position
+    private GameObject stage;                           //Stage
 
     /// <summary>
     /// GUI配置
@@ -49,15 +47,12 @@ public class MapEditor : EditorWindow
         EditorGUILayout.LabelField("マップ エディター");
 
         currentMode = GUILayout.Toolbar(currentMode,
-            new string[] { "マップ編集", "マップチップ", "書出し / 読み込み" });
+            new string[] { "マップ編集", "書出し / 読み込み" });
 
         switch ((Mode)currentMode)                      //モードにより機能が変わる
         {
             case Mode.EditMode:                         //マップ編集モード
                 EditMap();
-                break;
-            case Mode.Chips:                            //マップチップの読み込み
-                MapChips();
                 break;
             case Mode.SaveLoad:                         //マップ保存や読み込み
                 SaveLoad();
@@ -77,87 +72,61 @@ public class MapEditor : EditorWindow
 
     private void EditMap()
     {
-        TileInfo();                                             //Mapの大きさを設定
-
         EditorGUILayout.BeginVertical(GUI.skin.box);
-        OpenEditWindow();                                       //Viewerを開く
-        ResizetMap();                                           //マップサイズ調整
-        NewMap();                                               //新しいマップ作成
+        OpenNewScene();                                //新しいSceneを作る
+        OpenPalette();                                 //マップチップを開く
         EditorGUILayout.EndVertical();
+    }
+
+    private void OpenNewScene()
+    {
+        if (!GUILayout.Button("新しいステージ"))                            //新しいステージボタン
+            return;
+
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();        //今のシーンを保存するか
+        Scene scene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single);       //新しいシーン
+        LoadPrefab();                                                       //Prefabをロードする
+        InitNewScene();                                                     //Sceneを初期化
+    }
+
+    /// <summary>
+    /// PrefabをLoadする
+    /// </summary>
+    private void LoadPrefab()
+    {
+        tileMapPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/Editor/Grid.prefab");
+        gameManagerPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/GameManager/GameManager.prefab");
+    }
+
+    private void InitNewScene()
+    {
+        GameObject gameManager = Instantiate(gameManagerPrefab, Vector3.zero, Quaternion.identity);             //GameManagerを作成
+        gameManager.name = "GameManager";                                   //名前修正
+
+        stage = new GameObject();                                           //StageのEmptyObject
+        stage.name = "Stage";                                               //名前修正
+
+        string[] layout = { "Last", "Back", "Default", "Front", "First" };  //名前
+        int stageZ = 1;
+        int interval = 3;                                                   //Layoutの間隔
+        for (int i = 0; i < layout.Length; i++)
+        {
+            GameObject grid = Instantiate(tileMapPrefab, Vector3.zero, Quaternion.identity, stage.transform);    //Gridを作成する
+            grid.transform.position = new Vector3(0, 0, interval * 2 - i * interval - stageZ);                  //Z軸修正
+            grid.name = layout[i] + "Grid";                                  //名前修正
+            grid.transform.GetChild(0).name = layout[i] + "Layout";          //名前修正
+        }
     }
 
     /// <summary>
     /// EditWindowを開く
     /// </summary>
-    private void OpenEditWindow()
+    private void OpenPalette()
     {
-        if (GUILayout.Button("Map Viewerを開く"))               //開くボタン
+        if (GUILayout.Button("Map Chipを開く"))                                      //開くボタン
         {
-            if (mapViewer == null)                              //開いてない場合
-            {
-                mapViewer = MapViewer.CreateMapViewer(this);    //開く
-                mapViewer.Initialize(tileSize, xTile, yTile);   //初期化
-            }
-            else
-            {
-                mapViewer.Focus();                              //Windowを最上階にする
-            }
+            EditorApplication.ExecuteMenuItem("Window/Tile Palette");                //Paletteを開く
         }
-    }
-
-    /// <summary>
-    /// マップの大きさを設定
-    /// </summary>
-    private void TileInfo()
-    {
-        EditorGUILayout.BeginVertical(GUI.skin.box);
-
-        EditorGUILayout.BeginHorizontal();                                           //横並び
-        EditorGUILayout.LabelField("グリッドサイズ：", GUILayout.Width(infoLength)); //説明欄
-        tileSize = EditorGUILayout.FloatField(tileSize);                             //入力欄
-        EditorGUILayout.EndHorizontal();                                             //横並び
-
-        EditorGUILayout.BeginHorizontal();                                           //横並び
-        EditorGUILayout.LabelField("横ブロック数：", GUILayout.Width(infoLength));   //説明欄
-        xTile = EditorGUILayout.IntField(xTile);                                     //入力欄
-        EditorGUILayout.EndHorizontal();                                             //横並び
-
-        EditorGUILayout.BeginHorizontal();                                           //横並び
-        EditorGUILayout.LabelField("縦ブロック数：", GUILayout.Width(infoLength));   //説明欄
-        yTile = EditorGUILayout.IntField(yTile);                                     //入力欄
-        EditorGUILayout.EndHorizontal();                                             //横並び
-
-        EditorGUILayout.EndVertical();
-    }
-
-    /// <summary>
-    /// Mapの大きさを適用
-    /// </summary>
-    private void ResizetMap()
-    {
-        if (!GUILayout.Button("マップサイズを適用"))                                 //リサイズボタン
-            return;
-
-        if (mapViewer == null)                                                       //Viewer開いてない場合
-            mapViewer = MapViewer.CreateMapViewer(this);                             //開く
-
-        mapViewer.Initialize(tileSize, xTile, yTile);                                //初期化
-        mapViewer.Focus();
-    }
-
-    /// <summary>
-    /// 新しいマップ作成
-    /// </summary>
-    private void NewMap()
-    {
-        if (!GUILayout.Button("新しいマップ作成"))                                   //マップチップ作成ボタン
-            return;
-
-        if (mapViewer == null)                                                       //Viewer開いてない場合
-            mapViewer = MapViewer.CreateMapViewer(this);                             //開く
-
-        mapViewer.NewMap(tileSize, xTile, yTile);                                    //新しく生成
-        mapViewer.Focus();
     }
 
     /// <summary>
@@ -173,7 +142,6 @@ public class MapEditor : EditorWindow
     {
         EditorGUILayout.BeginVertical(GUI.skin.box);
         EditorGUILayout.LabelField("読み込み");
-
         EditorGUILayout.EndVertical();
     }
 
@@ -245,75 +213,5 @@ public class MapEditor : EditorWindow
 
         path = "Assets/";                                                           //プロジェクト以外はパス修正
         Refresh();
-    }
-
-    private void MapChips()
-    {
-        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUI.skin.box);
-        DrawMapChip();                                                              //MapChip一覧を描画
-        EditorGUILayout.EndScrollView();
-    }
-
-    /// <summary>
-    /// マップチップ一覧
-    /// </summary>
-    private void DrawMapChip()
-    {
-        string path = "Assets/MapChip";                                             //アセットパス
-        string[] textureNames = Directory.GetFiles(path, "*.png");                  //Pngファイルを探す
-
-        float xPos = 0.0f;                                      //セルの左上X座標
-        float yPos = 0.0f;                                      //セルの左上Y座標
-        float cellWidth = 100.0f;                               //セルの横サイズ
-        float cellHeight = 100.0f;                              //セルの縦サイズ
-        int maxCell = 5;                                        //セルの縦サイズ
-
-        EditorGUILayout.BeginVertical(GUI.skin.box);
-        #region Null Chip
-        EditorGUILayout.BeginHorizontal();                      //横並び始め
-        if (GUILayout.Button("Null", GUILayout.MaxWidth(cellWidth), GUILayout.MaxHeight(cellHeight),
-                GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false)))                           //ボタン設置（テクスチャ表示）
-            currentTilePath = "Null";                           //クリックされたらテクスチャ指定
-        xPos += cellWidth;                                      //X座標を右にずらす
-        #endregion
-
-        foreach (string textureDir in textureNames)             //アセットを一周する
-        {
-            if (xPos > (maxCell - 1) * cellWidth)               //横の数を超えた場合（0から始まるので-1）
-            {
-                xPos = 0.0f;                                    //X座標をリセット
-                yPos += cellHeight;                             //Y座標を一段下にずらす
-                EditorGUILayout.EndHorizontal();                //横並び終了
-            }
-
-            if (xPos == 0.0f)                                   //最初のセルの場合
-                EditorGUILayout.BeginHorizontal();              //横並び始め
-
-            GUILayout.FlexibleSpace();
-            Texture2D tex = (Texture2D)AssetDatabase.LoadAssetAtPath(textureDir, typeof(Texture2D));    //テクスチャを読み込む
-
-            if (GUILayout.Button(tex, GUILayout.MaxWidth(cellWidth), GUILayout.MaxHeight(cellHeight),
-                GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false)))                           //ボタン設置（テクスチャ表示）
-                currentTilePath = textureDir;                   //クリックされたらテクスチャ指定
-            GUILayout.FlexibleSpace();
-            xPos += cellWidth;                                  //X座標を右にずらす
-        }
-        EditorGUILayout.EndHorizontal();
-
-        EditorGUILayout.LabelField("Current Tile：" + currentTilePath);                                //現在選択しているチップ
-        if (currentTilePath != "Null" || currentTilePath != "")                                        //選択している場合
-        {
-            Texture2D currentTile = (Texture2D)AssetDatabase.LoadAssetAtPath(currentTilePath, typeof(Texture2D));
-            GUILayout.Box(currentTile);
-        }
-    }
-
-    /// <summary>
-    /// 現在選択のチップ
-    /// </summary>
-    /// <returns></returns>
-    public string CurrentChip()
-    {
-        return currentTilePath;
     }
 }
