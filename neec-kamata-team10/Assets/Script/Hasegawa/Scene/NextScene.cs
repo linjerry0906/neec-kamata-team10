d8655b27980fd345b99258738b020b8dd65f1fcc
はせ <->
2018-04-23 14:37:03 +0900

    シーン管理

---
+++ neec-kamata-team10/Assets/Script/Hasegawa/Scene/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScene : MonoBehaviour {
+
+    [SerializeField]
+    private EScene nextScene;
+
+    public void Change()
+    {
+        GameManager.Instance.GetComponent<Transform>().GetChild(0).GetComponent<SceneChange>().ChangeScene(nextScene);
+    }
+
+    void OnTriggerEnter(Collider t)
+    {
+        Debug.Log("in");
+        Change();
+    }
+
+}
