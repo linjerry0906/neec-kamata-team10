d9eb94aaeb7de173c9407f32dfceed832a37462e
はせ <->
2018-04-26 15:07:47 +0900

    プレイヤーの向きを取得するメソッドの追加

--- neec-kamata-team10/Assets/Script/Hasegawa/Character/Player.cs
+++ neec-kamata-team10/Assets/Script/Hasegawa/Character/Player.cs
@@ -12,25 +12,23 @@ public class Player : MonoBehaviour
     private float maxSpeed;
     [SerializeField]
     private float acceleration;
-    [SerializeField]
-    private Mirror mirror;
-    [SerializeField]
-    private float changeTime;
+    //[SerializeField]
+    //private Mirror mirror;
+    //[SerializeField]
+    //private float changeTime;
 
     private float speed = 0;
     private float storeDirectionX= 0;
     private Vector3 direction = new Vector3(1, 0, 0);
 
-    private ChangeScale changeScale;
+    //private ChangeScale changeScale;
     private ICharacterController controller;
 
     // Use this for initialization
     void Start()
     {
         controller = gameManager.GetComponent<GameManager>().GetController(eController);
-        changeScale = new ChangeScale(mirror, changeTime);
-        //changeScale.SetMirror(mirror);
-        //changeScale.SetChangeTime(changeTime);
+        //changeScale = new ChangeScale(mirror, changeTime);
     }
 
     // Update is called once per frame
@@ -38,6 +36,7 @@ public class Player : MonoBehaviour
     {
         Move();
         ChangeScale();
+        Debug.Log(GetDirection());
     }
 
     void Move()
@@ -67,6 +66,12 @@ public class Player : MonoBehaviour
 
     void ChangeScale()
     {
-        transform.localScale = changeScale.Scale(new Vector2(transform.position.x, transform.position.y));
+        //transform.localScale = changeScale.Scale(new Vector2(transform.position.x, transform.position.y));
+    }
+
+    public EDirection GetDirection()
+    {
+        if (direction.x < 0) return EDirection.LEFT;
+        else return EDirection.RIGHT;
     }
 }
---
+++ neec-kamata-team10/Assets/Script/Hasegawa/Character/TestObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour {
+    [SerializeField]
+    private Mirror mirror;
+    [SerializeField]
+    private float changeTime;
+
+    private ChangeScale changeScale;
+
+    // Use this for initialization
+    void Start()
+    {
+        changeScale = new ChangeScale(mirror, changeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeScale();
+    }
+
+    void ChangeScale()
+    {
+        transform.localScale = changeScale.Scale(new Vector2(transform.position.x, transform.position.y));
+    }
+
+    void OnTriggerEnter(Collider t)
+    {
+        if (t.gameObject.CompareTag("mirror"))
+        {
+            changeScale.SetMirror(t.gameObject.GetComponent<Mirror>());
+            //t.gameObject.GetComponent<Mirror>().
+        }
+    }
+}
