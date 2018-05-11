be4e1feca5b6c23250d1fec7ab8eb6e6b0ded5a4
はせ <->
2018-05-11 10:31:19 +0900

    プレイヤーの移動処理の修正 ジャンプ処理の修正

--- neec-kamata-team10/Assets/Script/Hasegawa/Character/Player.cs
+++ neec-kamata-team10/Assets/Script/Hasegawa/Character/Player.cs
@@ -6,16 +6,21 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private EController eController;
-    [SerializeField]
-    private float maxSpeed;
-    [SerializeField]
-    private float acceleration;
+    //[SerializeField]
+    //private float maxSpeed;
+    //[SerializeField]
+    //private float acceleration;
     [SerializeField]
     private float jumpPower;
 
-    private bool isJump = false;
-    private float speed = 0;
-    private float storeDirectionX= 0;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float moveForceMultiplier;
+
+    private bool isJump = true;
+    //private float speed = 0;
+    //private float storeDirectionX= 0;
     private Vector3 direction = new Vector3(0, 0, 0);
 
     private ICharacterController controller;
@@ -29,23 +34,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        //Move();
         Jump();
     }
 
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    //void Move()
+    //{
+    //    float directionX = controller.HorizontalMove().x;
+    //    ChangeSpeed(directionX);
+    //    ChangeDirection(directionX);
+    //
+    //    //Rigidbodyのvelocity.xだけを変更する
+    //    Vector3 velocity= direction * speed;
+    //    Vector3 temp = GetComponent<Rigidbody>().velocity;
+    //    temp.x = velocity.x;
+    //    GetComponent<Rigidbody>().velocity = temp;
+    //    //GetComponent<Rigidbody>().AddForce(100 * (directionX - temp.x), 0, 0);
+    //    //transform.position += direction * speed * Time.deltaTime;
+    //}
+
     void Move()
     {
-        float directionX = controller.HorizontalMove().x;
-        ChangeSpeed(directionX);
-        ChangeDirection(directionX);
+        Vector3 moveVector = Vector3.zero;
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
 
-        //Rigidbodyのvelocity.xだけを変更する
-        Vector3 velocity= direction * speed;
-        Vector3 temp = GetComponent<Rigidbody>().velocity;
-        temp.x = velocity.x;
-        GetComponent<Rigidbody>().velocity = temp;
-        //GetComponent<Rigidbody>().AddForce(100 * (directionX - temp.x), 0, 0);
-        //transform.position += direction * speed * Time.deltaTime;
+        direction = controller.HorizontalMove();
+        moveVector = moveSpeed * direction;
+
+        Vector3 velocity = new Vector3(rigidbody.velocity.x, 0, 0);
+        rigidbody.AddForce(moveForceMultiplier * (moveVector - velocity));
     }
 
     void Jump()
@@ -56,36 +78,51 @@ public class Player : MonoBehaviour
             GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             isJump = true;
         }
-    }
 
-    void OnCollisionEnter(Collision t)
-    {
-        if (t.gameObject.CompareTag("stage_block"))
+        if (isJump)
         {
-          //Debug.Log("stageに衝突");
-          isJump = false;
+            //ジャンプ中はRotationを固定
+            GetComponent<Rigidbody>().constraints =
+                RigidbodyConstraints.FreezeRotationX |
+                RigidbodyConstraints.FreezeRotationY |
+                RigidbodyConstraints.FreezeRotationZ;
+        }
+        else
+        {
+            Debug.Log("Yを固定");
+            //移動中はYとRotationを固定
+            GetComponent<Rigidbody>().constraints =
+                RigidbodyConstraints.FreezePositionY |
+                RigidbodyConstraints.FreezeRotationX |
+                RigidbodyConstraints.FreezeRotationY |
+                RigidbodyConstraints.FreezeRotationZ;
         }
     }
 
-    void ChangeDirection(float directionX)
-    {
-        if (directionX != 0) direction.x = directionX;
-    }
-
-    void ChangeSpeed(float directionX)
-    {
-        if (directionX != storeDirectionX && directionX != 0)  speed = 0;
-        storeDirectionX = directionX;
-
-        if (directionX != 0) speed += acceleration;
-        else speed -= acceleration;
-
-        speed = Mathf.Clamp(speed, 0, maxSpeed);
-    }
+    //void ChangeDirection(float directionX)
+    //{
+    //    if (directionX != 0) direction.x = directionX;
+    //}
+    //
+    //void ChangeSpeed(float directionX)
+    //{
+    //    if (directionX != storeDirectionX && directionX != 0)  speed = 0;
+    //    storeDirectionX = directionX;
+    //
+    //    if (directionX != 0) speed += acceleration;
+    //    else speed -= acceleration;
+    //
+    //    speed = Mathf.Clamp(speed, 0, maxSpeed);
+    //}
 
     public EDirection GetDirection()
     {
         if (direction.x < 0) return EDirection.LEFT;
         else return EDirection.RIGHT;
+    }
+
+    public void SetIsJump(bool isJump)
+    {
+        this.isJump = isJump;
     }
 }
