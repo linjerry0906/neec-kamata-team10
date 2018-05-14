d55c238313b7223ed0fe5c037e884e0bd817d113
okamotoeiju <okamoto.k017a1495@gmail.com>
2018-05-14 15:32:31 +0900

    トゲの追加

---
+++ neec-kamata-team10/Assets/Script/Okamoto/Splinter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splinter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //他のコライダと接触した時
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.transform.tag == "Player")
+        {
+            col.gameObject.GetComponent<AliveFlag>().Dead();
+        }
+    }
+}
