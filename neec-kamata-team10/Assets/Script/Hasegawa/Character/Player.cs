d0f9abde16cb7972dc306e9af259fc46a7148751
はせ <->
2018-05-18 14:48:11 +0900

    Playerのstateを修正

--- neec-kamata-team10/Assets/Script/Hasegawa/Character/Player.cs
+++ neec-kamata-team10/Assets/Script/Hasegawa/Character/Player.cs
@@ -22,7 +22,9 @@ public class Player : MonoBehaviour
     private Vector3 direction = new Vector3(1, 0, 0);
 
     private ICharacterController controller;
+
     private EPlayerState state = EPlayerState.Jump;
+    private EPlayerState stateStorage = EPlayerState.Move;
 
     private RigidbodyConstraints freezeY;
     private RigidbodyConstraints normal;
@@ -50,7 +52,9 @@ public class Player : MonoBehaviour
         Climb();
         Jump();
         Move();
+        CheckStay();
         FreezePosition();
+        CheckState();
     }
 
     void FixedUpdate()
@@ -62,7 +66,9 @@ public class Player : MonoBehaviour
         Vector3 moveVector = Vector3.zero;
         Rigidbody rigidbody = GetComponent<Rigidbody>();
 
-        if (controller.HorizontalMove() != Vector3.zero) direction = controller.HorizontalMove();
+        ChangeDirection();
+        //if (transform.localScale.x > 0 && direction.x < 0) transform.localScale.x *= direction.x;
+
         moveVector = moveSpeed * controller.HorizontalMove();
 
         Vector3 velocity = new Vector3(rigidbody.velocity.x, 0, 0);
@@ -75,6 +81,7 @@ public class Player : MonoBehaviour
         {
             GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             isJump = true;
+            state = EPlayerState.Jump;
         }
     }
 
@@ -91,7 +98,7 @@ public class Player : MonoBehaviour
     {
        if (GetComponent<Rigidbody>().velocity.y == 0)
        {
-           Debug.Log("Yを固定");
+            //Debug.Log("Yを固定");
             //移動中はYとZとRotationを固定
             GetComponent<Rigidbody>().constraints = freezeY;
                
@@ -129,8 +136,38 @@ public class Player : MonoBehaviour
         //    state = EPlayerState.Jump;
         //    SetIsClimb(false);
         //}
-        state = EPlayerState.Jump;
+        //state = EPlayerState.Jump;
         SetIsClimb(false);
+    }
+
+    void ChangeDirection()
+    {
+        //進んでいた方向の保存
+        if (controller.HorizontalMove() != Vector3.zero) direction = controller.HorizontalMove();
+
+        //transform.xとdirection.xを比べて方向が同じだったらreturnする
+        if ((transform.localScale.x > 0 && direction.x > 0) ||
+            (transform.localScale.x < 0 && direction.x < 0)) return;
+        
+        //returnされなかったら方向転換する
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;    
+    }
+
+    void CheckState()
+    {
+        if (state == stateStorage) return;
+        GetComponent<PlayerAnime>().ChangeState(state);
+        stateStorage = state;
+        Debug.Log(state);
+    }
+
+    void CheckStay()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb.velocity.x < 1 && rb.velocity.x > -1 && rb.velocity.y == 0) 
+            state = EPlayerState.Stay;
     }
 
     public EDirection GetDirection()
