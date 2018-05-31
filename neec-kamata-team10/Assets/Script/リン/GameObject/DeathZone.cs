e34fab3ad0b5fa3eb5fbba9933e341c6adfffa75
リン <linjerry0906@gmail.com>
2018-05-31 09:33:38 +0900

    DeathZone追加

---
+++ neec-kamata-team10/Assets/Script/リン/GameObject/DeathZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+            other.GetComponent<AliveFlag>().Dead();
+        if(other.tag == "Enemy")
+            Destroy(other.gameObject);
+    }
+}
