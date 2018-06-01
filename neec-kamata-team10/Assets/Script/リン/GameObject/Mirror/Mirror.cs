385ae3526405baf33d022993ad4b45b7d49db7d1
リン <linjerry0906@gmail.com>
2018-06-01 11:55:40 +0900

    鏡側の1Animation修正、背景Shaderに光が影響されるよう修正

--- neec-kamata-team10/Assets/Script/リン/GameObject/Mirror/Mirror.cs
+++ neec-kamata-team10/Assets/Script/リン/GameObject/Mirror/Mirror.cs
@@ -160,6 +160,8 @@ public class Mirror : MonoBehaviour
         if (r) Destroy(r);
         Collider c = obj.GetComponent<Collider>();                            //Collider
         if (c) Destroy(c);
+        Animator a = obj.GetComponent<Animator>();
+        if (a) Destroy(a);
     }
 
     /// <summary>
