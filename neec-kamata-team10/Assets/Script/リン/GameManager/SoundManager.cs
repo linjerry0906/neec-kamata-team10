9051497dd11bfaaea502e2f628b9ff8490b7f141
リン <linjerry0906@gmail.com>
2018-06-08 16:01:46 +0900

    Block作成

--- neec-kamata-team10/Assets/Script/リン/GameManager/SoundManager.cs
+++ neec-kamata-team10/Assets/Script/リン/GameManager/SoundManager.cs
@@ -10,26 +10,28 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour {
 
     [SerializeField]
-    private List<AudioClip> bgmsSource;             //音源
-    [SerializeField]
     private List<AudioClip> sesSource;              //効果音源
 
     private Dictionary<string, AudioClip> bgms;     //BGM
     private Dictionary<string, AudioClip> ses;      //SE
 
-    private string currentBgm;
+    private string currentBgm = "";
 
     private void Start()
     {
-        //Dictionaryに追加
-        for (int i = 0; i < bgmsSource.Count; ++i)
-        {
-            bgms.Add(bgmsSource[i].name, bgmsSource[i]);
-        }
-
         for (int i = 0; i < sesSource.Count; ++i)
         {
             ses.Add(sesSource[i].name, sesSource[i]);
         }
+    }
+
+    public void PlayBGM(string name)
+    {
+        if (currentBgm.Equals(name))
+            return;
+
+
+
+        currentBgm = name;
     }
 }
