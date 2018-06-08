e7182d255ff2cfc96fe2eb6b59c755137fd6ac3d
リン <linjerry0906@gmail.com>
2018-06-08 09:36:52 +0900

    エディター調整

--- neec-kamata-team10/Assets/Script/リン/Editor/LayoutManager.cs
+++ neec-kamata-team10/Assets/Script/リン/Editor/LayoutManager.cs
@@ -120,7 +120,8 @@ public class LayoutManager
             canvas = GameObject.Find("Canvas");
         }
 
-        canvas.GetComponent<Canvas>().worldCamera = mainCamera.GetComponent<Camera>();
+        canvas.GetComponent<Canvas>().worldCamera = mainCamera.transform.GetChild(1).GetComponent<Camera>();
+        mainCamera.GetComponent<CameraWork>().SetTarget(GameObject.FindGameObjectWithTag("Player"));
         GameObject mirrorUI = canvas.transform.GetChild(0).GetChild(4).GetChild(1).gameObject;
         gameSystem.GetComponent<MirrorSetting>().SetMirrorUI(mirrorUI);
     }
