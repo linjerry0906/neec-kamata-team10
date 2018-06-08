305f37faa79b63bc7ac7babdf87be5f01183a7af
はせ <->
2018-06-08 12:01:27 +0900

    RespawnPointの作成 PlayerRespawnの追加

---
+++ neec-kamata-team10/Assets/Script/Hasegawa/Character/PlayerRespawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    private Vector2 respawnPos;
+    private int num = 0;
+
+    void Start()
+    {
+        respawnPos = transform.parent.position;
+    }
+     
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.CompareTag("Respawn"))
+        {
+            int spawnNum = c.gameObject.GetComponent<RespawnPoint>().GetNumber();
+
+            if (spawnNum < num) return;
+            num = spawnNum;
+            respawnPos = c.transform.position;
+        }
+    }
+
+    Vector2 GetRespawnPosition()
+    {
+        return respawnPos;
+    }
+}
