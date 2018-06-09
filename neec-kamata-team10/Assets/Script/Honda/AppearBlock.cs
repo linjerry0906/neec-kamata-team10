af5bb1eaa44a0916a635fb44ce596ee269da195e
NaohiroHonda <39308876NaohiroHonda@users.noreply.github.com>
2018-06-09 11:20:56 +0900

    MOSの授業が暇だったのでスイッチとそれに連動して出現したり消えたりするBlockを試作した ひねくれたコードになってる

---
+++ neec-kamata-team10/Assets/Script/Honda/AppearBlock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearBlock : MonoBehaviour
+{
+    public bool IsReverseAppear = false; //trueだとスイッチOnで消滅、スイッチOffで出現
+
+    [SerializeField]
+    public SwitchObject switchObj;
+
+    public new GameObject gameObject;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ///<summary>
+        /// アクティブ状態をスイッチから判断
+        /// Activeの条件は
+        /// 
+        ///                 |  switch-On?
+        ///      IsReverse? | false | true
+        /// ----------------+-------+-------
+        ///  normal = false | false | true
+        /// reverse =  true | true  | false
+        /// 
+        /// なのでXORで設定できる
+        ///</summary>
+
+        gameObject.SetActive(switchObj.IsTurnOn ^ IsReverseAppear);
+	}
+}
