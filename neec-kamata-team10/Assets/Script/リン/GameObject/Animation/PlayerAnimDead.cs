b0b08db7b13983974b685b4febd98ac90628c6dc
リン <linjerry0906@gmail.com>
2018-06-11 10:21:28 +0900

    PlayerDeathAnimation

--- neec-kamata-team10/Assets/Script/リン/GameObject/Animation/PlayerAnimDead.cs
+++ neec-kamata-team10/Assets/Script/リン/GameObject/Animation/PlayerAnimDead.cs
@@ -1,5 +1,5 @@
 ﻿//------------------------------------------------------
-// 作成日：2018.5.18
+// 作成日：2018.6.11
 // 作成者：林 佳叡
 // 内容：プレイヤー死んだ時のアニメーション
 //------------------------------------------------------
@@ -8,8 +8,11 @@ using UnityEngine;
 
 public class PlayerAnimDead : IAnimeState
 {
+    private Animator animator;
+
     public void Execute(Animator animator)
     {
+        this.animator = animator;
         animator.SetTrigger("isDead");
     }
 
@@ -25,6 +28,10 @@ public class PlayerAnimDead : IAnimeState
 
     void IAnimeState.Update()
     {
-        return;
+        if (!animator.GetBehaviour<AnimeScriptDead>().IsEnd())
+            return;
+        //終了したらステージ再開
+        GameManager.Instance.GetStageManager().EndStage();
+        GameManager.Instance.GetStageManager().SetClear(false);
     }
 }
