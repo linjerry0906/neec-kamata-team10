375b7c7e33eb0a1903b5f224b29aae37071d022e
リン <linjerry0906@gmail.com>
2018-06-20 10:11:55 +0900

    BreakEffect追加

---
+++ neec-kamata-team10/Assets/Script/リン/GameObject/Block/BreakBlockEffect.cs
@@ -0,0 +1,19 @@
+﻿//------------------------------------------------------
+// 作成日：2018.6.20
+// 作成者：林 佳叡
+// 内容：Breakされたときのエフェクト
+//------------------------------------------------------
+using UnityEngine;
+
+public class BreakBlockEffect : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject particle;                    //EffectPrefab
+
+    private void OnDestroy()
+    {
+        GameObject effect = Instantiate(particle, transform.position, Quaternion.identity);
+        float lifeTime = effect.GetComponent<ParticleSystem>().main.startLifetime.constantMax;
+        Destroy(effect, lifeTime);                  //LifeTimeに合わせて消す
+    }
+}
