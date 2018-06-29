2928d70cc7c011038b8e6c3ce34e3eaaede08b38
リン <linjerry0906@gmail.com>
2018-06-29 13:03:40 +0900

    Stage9追加

--- neec-kamata-team10/Assets/Script/Hasegawa/Scene/EScene.cs
+++ neec-kamata-team10/Assets/Script/Hasegawa/Scene/EScene.cs
@@ -2,6 +2,13 @@
 {
     Stage1 = 1,
     Stage2 = 2,
+    Stage3 = 3,
+    Stage4 = 4,
+    Stage5 = 5,
+    Stage6 = 6,
+    Stage7 = 7,
+    Stage8 = 8,
+    Stage9 = 9,
     StageNull,              //Stage追加はこの上に
     TitleTest = -100,
     PlayTest,
