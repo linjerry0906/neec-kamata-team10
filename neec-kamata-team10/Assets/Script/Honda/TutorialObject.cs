86da06015d35f9e89f1f917d4e808bbe281590b5
NaohiroHonda <39308876NaohiroHonda@users.noreply.github.com>
2018-07-09 11:54:48 +0900

    チュートリアルメッセージにフェード追加

--- neec-kamata-team10/Assets/Script/Honda/TutorialObject.cs
+++ neec-kamata-team10/Assets/Script/Honda/TutorialObject.cs
@@ -13,18 +13,64 @@ using UnityEngine.UI;
 public class TutorialObject : MonoBehaviour {
 
     [SerializeField]
-    public Text tutorialText; //テキストUI
+    private Text tutorialText;    //テキストUI
+    [SerializeField]
+    private string message;       //メッセージ内容
+    [SerializeField]
+    private float fadeTime = 0.5f;  //フェードタイム
 
-    public string message; //メッセージ内容
+    private Color color;          //フォントのColor値
+
+    private enum TextNow
+    {
+        none,
+        fadeIn,
+        appear,
+        fadeOut
+    }
+
+    private TextNow textNow;      //テキストの表示ステータス
 
 	// Use this for initialization
 	void Start () {
-		
+        textNow = TextNow.none;
+
+        color = tutorialText.color;
+        color.a = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (textNow == TextNow.none || textNow == TextNow.appear) return; //変化がない場合はreturn
+
+        //変化がある場合
+        //else
+		if (textNow == TextNow.fadeIn)
+        {
+            color.a += Time.deltaTime / fadeTime;
+
+            //In終わったら
+            if (color.a >= 1f)
+            {
+                color.a = 1f;
+                textNow = TextNow.appear;
+            }
+        }
+
+        else //if (textNow == TextNow.fadeOut)
+        {
+            color.a -= Time.deltaTime / fadeTime;
+
+            //Out終わったら このタイミングでText消す
+            if (color.a <= 0f)
+            {
+                color.a = 0f;
+                textNow = TextNow.none;
+                tutorialText.text = "";              //テキストUIの中身を消去
+            }
+        }
+
+        tutorialText.color = color;
 	}
 
     void OnTriggerEnter(Collider other) //プレイヤーと接触したらTextを変更 Stayだと処理が重そう
@@ -33,7 +79,8 @@ public class TutorialObject : MonoBehaviour {
 
         else                            //プレーヤーと接触したら
         {
-            tutorialText.text = message;         //テキストUIの中身を設定されたmessageに変更
+            tutorialText.text = message;         //共通テキストUIの中身を設定されたmessageに変更
+            textNow = TextNow.fadeIn;            //fadeInに変更
         }
     }
 
@@ -43,7 +90,7 @@ public class TutorialObject : MonoBehaviour {
 
         else                            //プレーヤーが離れた場合
         {
-            tutorialText.text = "";              //テキストUIの中身を消去
+            textNow = TextNow.fadeOut;
         }
     }
 }
