f62c06c327e9a22aedcc658b7cad4d38f628bfc8
うちの <ouyong16@gmail.com>
2018-07-09 12:07:26 +0900

    ゲーム終了ボタン（コントローラーのみ）の追加

--- neec-kamata-team10/Assets/Script/Uchino/Script/Controller/ICharacterlController.cs
+++ neec-kamata-team10/Assets/Script/Uchino/Script/Controller/ICharacterlController.cs
@@ -11,13 +11,14 @@ public interface ICharacterController
     bool SwitchToTheLeft();           //鏡を左に切り替える
     bool OperateTheMirror();          //鏡の操作
     bool ThrowMirror();               //鏡を投げる（キーを離したとき:true）
-	bool Pause();					  //一時停止s:
+	bool Pause();					  //一時停止:
 
     bool MoveSelectionUp();           //選択項目を上に移動
     bool MoveSelectionDown();         //選択項目を下に移動
     bool MoveSelectionLeft();         //選択項目を左に移動
     bool MoveSelectionRight();        //選択項目を右に移動
     void Update();                    //選択状態の更新
+    bool GameEnd();                   //ゲームを終了
 
     void CountFlameInit();            //フレームカウント数を初期化する
     void CountFlameUpdate();          //押しているフレーム数を数える
