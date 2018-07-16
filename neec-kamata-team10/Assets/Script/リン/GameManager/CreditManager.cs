0d40f8af902179b8c6faceba148a921bbde71b37
リン <linjerry0906@gmail.com>
2018-07-16 11:02:04 +0900

    CreditScene追加

---
+++ neec-kamata-team10/Assets/Script/リン/GameManager/CreditManager.cs
@@ -0,0 +1,21 @@
+﻿//------------------------------------------------------
+// 作成日：2018.7.16
+// 作成者：林 佳叡
+// 内容：クレジット画面の管理者
+//------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditManager : MonoBehaviour 
+{
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
