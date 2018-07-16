157a02b2ca40255adc65ed9a18a48527a0840ad4
NaohiroHonda <39308876NaohiroHonda@users.noreply.github.com>
2018-07-16 15:50:56 +0900

    Revert "Group一括操作用のAppearBlockと親Empty用のScriptを作成(今更)"
    This reverts commit c5d89c1c4d2c3b1676db129732ee1994045dd1cb.

--- neec-kamata-team10/Assets/Script/Honda/AppearBlock.cs
+++ neec-kamata-team10/Assets/Script/Honda/AppearBlock.cs
@@ -17,38 +17,38 @@ using UnityEngine;
 ///     
 /// </summary>
 
-public abstract class AppearBlock : MonoBehaviour
+public class AppearBlock : MonoBehaviour
 {
-    protected bool IsReverseAppear;
-    protected bool isActive;           //現在のActive状態
-    protected bool newActive;          //Update時の状態判定用bool
-
-    protected float fadeTime;          //fade時間
-    protected float timeNow;           //経過時間
+    public bool IsReverseAppear = false; //trueだとスイッチOnで消滅、スイッチOffで出現
+    bool isActive;                       //現在のActive状態
 
     [SerializeField]
-    protected GameObject appearObject; //スイッチで切り替えさせるObject
+    public SwitchObject switchObj; //スイッチ本体(のコード)
+    public GameObject appearObject; //スイッチで切り替えさせるObject
+
+    //FadeTime関連
+    [SerializeField]
+    private float fadeTime = 0.2f;  //fade時間
+    private float timeNow;          //経過時間
 
     //FadeColor関連
-    protected Material objectMaterial;     //fade操作に使うObjectのMaterial
-    protected Color colorNow, startColor;  //現在の色、Fade開始時の色
-    protected Color maxColor, clearColor;  //α=max,0 Fade後の色
+    private Material objectMaterial;     //fade操作に使うObjectのMaterial
+    private Color colorNow, startColor;  //現在の色、Fade開始時の色
+    private Color maxColor, clearColor;  //α=max,0 Fade後の色
 
 
-    protected enum fadeStatus //fade状態 していない,in,out
+    enum fadeStatus //fade状態 していない,in,out
     {
         none,
         fadein,
         fadeout
     }
 
-    protected fadeStatus fadeNow;
+    fadeStatus fadeNow;
 
 	// Use this for initialization
-	protected void OriginStart ()
-    {
+	void Start () {
         isActive = IsReverseAppear;        //初期状態の設定
-
         appearObject.SetActive(isActive);
 
         objectMaterial = appearObject.GetComponent<Renderer>().material;
@@ -67,7 +67,7 @@ public abstract class AppearBlock : MonoBehaviour
     }
 	
 	// Update is called once per frame
-	protected void OriginUpdate () {
+	void Update () {
 
         if (fadeNow != fadeStatus.none)   //タイマーの時間分岐
         {
@@ -88,6 +88,8 @@ public abstract class AppearBlock : MonoBehaviour
         /// なのでXORで設定できる
         ///</summary>
 
+        //現在の状態と新しい状態を比較 違う=XORがtrue
+        bool newActive = switchObj.IsTurnOn ^ IsReverseAppear;
 
         if (isActive ^ newActive)
         {
@@ -150,6 +152,4 @@ public abstract class AppearBlock : MonoBehaviour
         if (timeNow > fadeTime) return 1f;
         return (timeNow / fadeTime);
     }
-
-    protected abstract void SetActive(bool isActive);
 }
