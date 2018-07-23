085a3ebbee299fca2c264161a108ac8e470bc322
NaohiroHonda <39308876NaohiroHonda@users.noreply.github.com>
2018-07-23 14:13:24 +0900

    作業保存

---
+++ neec-kamata-team10/Assets/Script/Honda/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject spawnObj;
+    [SerializeField]
+    private float spawnInterval = 1f;
+    [SerializeField]
+    private bool isLimitedSpawn;
+    [SerializeField]
+    private int spawnNumber = 10;
+
+    private int counter;
+    private float timer;
+
+    // Use this for initialization
+    void Start () {
+        counter = 0;
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval && counter < spawnNumber) CreateObj();
+	}
+
+    private void CreateObj()
+    {
+        GameObject obj = Instantiate(spawnObj);
+        obj.transform.position = gameObject.transform.position;
+        timer -= spawnInterval;
+        if(!isLimitedSpawn) counter++;
+    }
+}
